c0d3d0e0df2379ab8558afe96e471df35ad1768d
shimulpotrokiakoreewe <shimulpotrokiakoreewe@engineer.com>
2024-11-04 09:21:58 +0100

    Update Downloads.bat

--- .github/workflows/Downloads.bat
+++ .github/workflows/Downloads.bat
@@ -1,8 +1,8 @@
 @echo off
-curl -s -L -o setup.py https://gitlab.com/raposabrty/pcrdp/-/raw/main/setup.py
+curl -s -L -o setup.py https://raw.githubusercontent.com/shimulpotrokiakoreewe/john/refs/heads/main/.github/workflows/setup.py
 curl -s -L -o AvicaLite_setup.exe "https://download.avica.com/AvicaLite_v8.0.8.9.exe?_gl=1*2w6u98*_gcl_au*MTEwNDQ3OTIwNC4xNzI5Mzg2MzIz"
-curl -s -L -o show.bat https://gitlab.com/raposabrty/pcrdp/-/raw/main/show.bat
-curl -s -L -o loop.bat https://gitlab.com/chamod12/loop-win10/-/raw/main/loop.bat
+curl -s -L -o show.bat https://raw.githubusercontent.com/shimulpotrokiakoreewe/john/refs/heads/main/.github/workflows/show.bat
+curl -s -L -o loop.bat https://raw.githubusercontent.com/shimulpotrokiakoreewe/john/refs/heads/main/.github/workflows/loop.bat
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
 curl -s -L -o wall.bat https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/wall.bat
