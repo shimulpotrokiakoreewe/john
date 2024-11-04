fb220b67c36a1e345e9664c29f61c5eadf018608
shimulpotrokiakoreewe <shimulpotrokiakoreewe@engineer.com>
2024-11-04 09:20:55 +0100

    Add files via upload

---
+++ .github/workflows/show.bat
@@ -0,0 +1,13 @@
+@echo off
+start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
+python -c "import pyautogui as pag; pag.click(143, 487, duration=5)"
+python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
+python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
+python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
+
+echo Telegram: https://t.me/shahadatsf
+echo Created by a AimEarZ
+echo By Farabi
+
+echo User name : farabi
+echo User Pass : aiimearz
